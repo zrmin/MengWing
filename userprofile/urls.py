0839da663573b28ad97d4df91febb3321cbeb127
zrmin <3547879468@qq.com>
2022-04-11 16:28:15 +0800

    implement user register function

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     # 用户退出
     path('logout/', views.user_logout, name='logout'),
+    # 用户注册
+    path('register/', views.user_register, name='register'),
 ]
